1774a62445bd20e070cbe7c616f35524da548840
ajbornacellyn <55602745+ajbornacellyn@users.noreply.github.com>
2022-11-17 19:13:39 -0500

    Update settings.py

--- retool/settings.py
+++ retool/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--z!+dd&2*x__r1ia8e+-kuk&zcqa74$25eo!jbtab+-!t73l(q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['django-retool-production.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
